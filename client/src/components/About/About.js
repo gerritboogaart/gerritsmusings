86fe0fe9637738abd8988a19613fcba0e420550b
Gerrit Boogaart <guituitdenboogaart@merkleinc.com>
2020-10-11 16:00:13 -0400

    update about add goodreads

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -5,12 +5,15 @@ import './About.css';
 export const About = () => {
 
   const linking = (type) => {
-    switch(type) {
+    switch (type) {
       case 'twitter':
         window.open('https://twitter.com/gerritb42', "_blank");
         break;
       case 'linkedin':
         window.open('https://www.linkedin.com/in/gerrit-boogaart-a8b56015/', '_blank');
+        break;
+      case 'goodreads':
+        window.open('https://www.goodreads.com/user/show/18523417/', '_blank');
         break;
       default:
         return <div />
@@ -22,7 +25,7 @@ export const About = () => {
       Gerrit Boogaart
       <div className='text-about-page'>
         Welcome to my portfolio. This site is written in React using hooks and sometimes context.<br />
-        My hobbies are coding in JavaScript, ReactJS, NodeJS, and playing with CSS3, HTML5 and MySql.<br />
+        I code in JavaScript, ReactJS, NodeJS, and use CSS3, HTML5 and MySql to bring apps alive<br />
         My hobbies that don't involve a screen are my dog Boomer (a Boston Terrier), reading my Kindle and indoor cycling.
         <br />
         <p />
@@ -32,11 +35,12 @@ export const About = () => {
           <p>'I would love to change the world, but they won’t give me the source code'</p>
           <p>'I write code, so my dog can have a better life'</p>
         </div>
-        </div>
-        <div className='social-icons'>
-          <Icon link size='big' className='social-links' name='linkedin' onClick={() => linking('linkedin')}/>
-          <Icon link size='big' className='social-links' name='twitter' onClick={() => linking('twitter')}/>
-        </div>
+      </div>
+      <div className='social-icons'>
+        <Icon link size='big' className='social-links' name='linkedin' onClick={() => linking('linkedin')} />
+        <Icon link size='big' className='social-links' name='twitter' onClick={() => linking('twitter')} />
+        <Icon link size='big' className='social-links' name='goodreads g' onClick={() => linking('goodreads')} />
+      </div>
     </div>
 
 
