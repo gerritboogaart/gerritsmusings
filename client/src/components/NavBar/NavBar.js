27b966f471148e7f904c3fbf16d1e9890d48f5a3
Gerrit Boogaart <guituitdenboogaart@merkleinc.com>
2020-10-11 15:28:42 -0400

    improve mobile css frontpage and blog

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { List } from 'semantic-ui-react';
+import { List, Icon } from 'semantic-ui-react';
 import './NavBar.css';
 
 export const NavBar = () => {
@@ -25,11 +25,11 @@ export const NavBar = () => {
   return (
     <div className='nav-bar'>
       <List horizontal>
-        <List.Item > <Link onClick={() => setActive('home')}  style={setClass('')} to="/">Home</Link></List.Item>
+        <List.Item > <Link onClick={() => setActive('home')}  style={setClass('')} to="/"><Icon name='home' /></Link></List.Item>
         <List.Item > <Link onClick={() => setActive('blog')} style={setClass('blog')}  to="/blog">Blog</Link> </List.Item>
         {/* <List.Item> <Link onClick={() => setActive('forms')} style={setClass('forms')}  to="/forms">Form Builder</Link> </List.Item> */}
-        <List.Item> <Link onClick={() => setActive('blocks')} style={setClass('blocks')}  to="/blocks">Blocks Game</Link> </List.Item>
-        <List.Item> <Link onClick={() => setActive('weather')} style={setClass('weather')}  to="/weather">Weather App</Link> </List.Item>
+        <List.Item> <Link onClick={() => setActive('blocks')} style={setClass('blocks')}  to="/blocks">Blocks</Link> </List.Item>
+        <List.Item> <Link onClick={() => setActive('weather')} style={setClass('weather')}  to="/weather">Weather</Link> </List.Item>
         <List.Item> <Link onClick={() => setActive('photos')} style={setClass('photos')}  to="/photos">Photos</Link> </List.Item>
         <List.Item> <Link onClick={() => setActive('about')} style={setClass('about')}  to="/about">About</Link> </List.Item>
       </List>
